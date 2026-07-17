f4f67ea166dbf090f9d7dc6770ab03634626c510
Andrew549484 <andrewgordon.tampa@gmail.com>
2026-07-17 16:09:50 -0400

    Getting firestore to work, updating gitignore again

--- src/app/home-page/home-page.ts
+++ src/app/home-page/home-page.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
-
+import { FirebaseService } from '../services/firebase';
 @Component({
   selector: 'app-home-page',
   imports: [],
   templateUrl: './home-page.html',
   styleUrl: './home-page.css',
 })
-export class HomePage {}
+export class HomePage {
+  constructor(private firebaseService: FirebaseService) {
+
+  }
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,21 +1,13 @@
 export const environment = {
     production: false,
-    firebaseConfig: {
-
-  apiKey: "AIzaSyAQP4YQlABL3nI0w1eC94SZSjLJf1gcldc",
-
-  authDomain: "sylvies-oneshots.firebaseapp.com",
-
-  projectId: "sylvies-oneshots",
-
-  storageBucket: "sylvies-oneshots.firebasestorage.app",
-
-  messagingSenderId: "273176202961",
-
-  appId: "1:273176202961:web:868b31b88db297ac513ff0",
-
-  measurementId: "G-QZFJRCHT0V"
-
-}
+    firebaseConfig:{
+    apiKey: "AIzaSyAQP4YQlABL3nI0w1eC94SZSjLJf1gcldc",
+    authDomain: "sylvies-oneshots.firebaseapp.com",
+    projectId: "sylvies-oneshots",
+    storageBucket: "sylvies-oneshots.firebasestorage.app",
+    messagingSenderId: "273176202961",
+    appId: "1:273176202961:web:868b31b88db297ac513ff0",
+    measurementId: "G-QZFJRCHT0V"
+  }
 
 };
