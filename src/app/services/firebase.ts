228ee5d5cd89b5010222be9d5850fac29dc56524
Andrew549484 <andrewgordon.tampa@gmail.com>
2026-07-17 16:11:40 -0400

    gitignore is weird

--- src/app/services/firebase.ts
+++ src/app/services/firebase.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { initializeApp, FirebaseApp } from 'firebase/app';
 import { getAuth, Auth } from 'firebase/auth';
 import { getFirestore, Firestore } from 'firebase/firestore';
-import { environment } from '../../environments/environment';
 import {  
   collection, 
   doc, 
